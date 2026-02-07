eaee13d3f1db60076e6528da2b17cbc38e57f17d
ki8yk8 <ki8yk8@gmail.com>
2026-02-07 15:36:06 +0545

    creates basic grid functions

--- src/grids.py
+++ src/grids.py
@@ -2,6 +2,8 @@
 creates utility function to work with grids like converting a given image to grid, visualizing it, etc.
 """
 import torch
+from torchvision.io import decode_image
+import matplotlib.pyplot as plt
 
 class Grid:
 	def __init__(self, height=128, width=128):
@@ -10,11 +12,22 @@ class Grid:
 		"""
 		self.grid = torch.zeros((16, height, width))
 
+	def copy_image(self, path, verbose=False):
+		image = decode_image(path, mode="RGB")
+		# resized_image = self.resize_image(image)
+
+		if verbose:
+			plt.imshow(torch.permute(image, (1, 2, 0)))
+			plt.tight_layout()
+			plt.axis("off")
+			plt.savefig("./outputs/hello.png", bbox_inches="tight")
+			plt.close()
+
 	def grid2img(self, path=None):
 		"""
 		use to convert the cellular automata grid into image.
 		"""
-		pass
+		alive_cells_mask = self.get_alive_mask(self.grid)
 
 	def get_alive_mask(self):
 		"""
--- train-automata.py
+++ train-automata.py
@@ -1,3 +1,12 @@
 """
 Script to train the neural cellular automata for creating neural rules that ultimately stores the image.
 """
+from src.grids import Grid
+
+IMAGE_PATH = "./images/banana-no-bg.png"
+
+# importing the image and creating the grid
+grid = Grid()
+grid.copy_image(IMAGE_PATH, verbose=True)
+
+# Grid.grid2img("./outputs/frame.png")
