400aec0106f8c65708873b157379c1ff2d6cade2
ki8yk8 <ki8yk8@gmail.com>
2026-03-01 00:51:56 +0545

    adds a training pipeline

--- train-automata.py
+++ train-automata.py
@@ -9,22 +9,47 @@ Model Training Steps
 5. Repeat this for n number of times where n is a random number between 64, 96
 6. Apply BPTT by averaging the l2 loss
 """
+import numpy as np
+import torch
+
 from src.grids import Grid
 from src.model import CellularNeuralAutomata
 
 IMAGE_PATH = "./images/banana-no-bg.png"
+EPOCHS = 100
 
-# importing the image and creating the grid
-grid = Grid()
+# importing the image and creating the grid: training grid is used for training while ground grid acts as the ground truth for computing the losses
+training_grid, true_grid = Grid(), Grid()
 
 # copies the image to grid
-grid.copy_image(IMAGE_PATH)
+training_grid.copy_image(IMAGE_PATH)
+true_grid.copy_image(IMAGE_PATH)
 
-# showing the initial image
-grid.grid2img("./outputs/frame.png")
-
-perception_vector = grid.get_perception_vector()
-
+# initializing model, optimizer, and criterion for the training
 model = CellularNeuralAutomata()
-updated_state = model(perception_vector)
-grid.update(updated_state)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+model.train()
+for i in range(EPOCHS):
+	optimizer.zero_grad()
+	# choose a random number of timesteps
+	n_timesteps = np.random.randint(low=64, high=96)
+
+	total_loss = 0.0 
+	for n in range(n_timesteps):
+		perception_vector = training_grid.get_perception_vector()
+		delta = model(perception_vector)
+		training_grid.update(delta)
+
+		current_image = training_grid.grid[:3,:,:]
+		true_image = true_grid.grid[:3,:,:]
+		
+		loss = criterion(current_image, true_image)
+		total_loss += loss
+
+	average_loss = total_loss/n_timesteps
+	average_loss.backward()
+	optimizer.step()
+
+	print(f"Epoch {i+1}, Loss: {average_loss.item()}")
